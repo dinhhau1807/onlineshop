389b66df00a995eb9e6375ab1a9b161a05550ba2
Dinh Hau <nguyendinhhau1807@gmail.com>
2019-06-29 14:43:36 +0700

    First commit

---
+++ Model/Dao/UserDao.cs
@@ -0,0 +1,64 @@
+﻿using Model.EF;
+using PagedList;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.Dao
+{
+    public class UserDao
+    {
+        OnlineShopDbContext _context = null;
+
+        public UserDao()
+        {
+            _context = new OnlineShopDbContext();
+        }
+
+        public long Insert(User entity)
+        {
+            _context.Users.Add(entity);
+            _context.SaveChanges();
+            return entity.ID;
+        }
+
+        public IEnumerable<User> ListAllPaging(int page, int pageSize)
+        {
+            return _context.Users.OrderBy(x => x.ID).ToPagedList(page, pageSize);
+        }
+
+        public User GetById(string userName)
+        {
+            return _context.Users.SingleOrDefault(x => x.UserName == userName);
+        }
+
+        public int Login(string userName, string passWord)
+        {
+            var result = _context.Users.SingleOrDefault(x => x.UserName == userName);
+            if (result == null)
+            {
+                return 0;
+            }
+            else
+            {
+                if (result.Status == false)
+                {
+                    return -1;
+                }
+                else
+                {
+                    if (result.Password == passWord)
+                    {
+                        return 1;
+                    }
+                    else
+                    {
+                        return -2;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ OnlineShop/Areas/Admin/Controllers/UserController.cs
@@ -0,0 +1,55 @@
+﻿using Model.Dao;
+using Model.EF;
+using OnlineShop.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PagedList;
+
+namespace OnlineShop.Areas.Admin.Controllers
+{
+    public class UserController : BaseController
+    {
+        // GET: Admin/User
+        public ActionResult Index(int page = 1, int pageSize = 10)
+        {
+            var dao = new UserDao();
+            var model = dao.ListAllPaging(page, pageSize);
+
+            return View(model);
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(User user)
+        {
+            if (ModelState.IsValid)
+            {
+                user.Password = Encryptor.MD5Hash(user.Password);
+
+                var dao = new UserDao();
+                long id = dao.Insert(user);
+
+                if (id > 0)
+                {
+                    return RedirectToAction("Index", "User");
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Thêm mới không thành công.");
+                }
+
+                return View("Index");
+            }
+            return View();
+        }
+    }
+}
