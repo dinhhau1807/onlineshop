c8f0c002d079da239b4d7a515409e07248e9dfc3
Dinh Hau <nguyendinhhau1807@gmail.com>
2019-07-05 21:57:35 +0700

    config CKEditor

--- OnlineShop/assets/admin/js/plugins/ckeditor/config.js
+++ OnlineShop/assets/admin/js/plugins/ckeditor/config.js
@@ -4,7 +4,19 @@
  */
 
 CKEDITOR.editorConfig = function( config ) {
-	// Define changes to default configuration here. For example:
-	// config.language = 'fr';
-	// config.uiColor = '#AADC6E';
+    // Define changes to default configuration here. For example:
+    // config.language = 'fr';
+    // config.uiColor = '#AADC6E';
+
+    config.syntaxhighlight_lang = 'csharp';
+    config.syntaxhighlight_hideControls = true;
+    config.lang = 'vi';
+    config.filebrowserBrowserUrl = '/assets/admin/js/plugins/ckfinder/ckfinder.html';
+    config.filebrowserImageBrowserUrl = '/assets/admin/js/plugins/ckfinder/ckfinder.html?Type=Images';
+    config.filebrowserFlashBrowserUrl = '/assets/admin/js/plugins/ckfinder/ckfinder.html?Type=Flash';
+    config.filebrowserUploadUrl = '/assets/admin/js/plugins/ckfinder/core/connector/aspx/connector.aspx?command=QuickUpload&type=Files';
+    config.filebrowserImageUploadUrl = '/Data';
+    config.filebrowserFlashUploadUrl = '/assets/admin/js/plugins/ckfinder/core/connector/aspx/connector.aspx?command=QuickUpload&type=Flash';
+
+    CKFinder.setupCKEditor(null, '/assets/admin/js/plugins/ckfinder/');
 };
