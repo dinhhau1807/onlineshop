eba76cd15eca437979a01d2e09e8a7219c5c9eb1
Dinh Hau <nguyendinhhau1807@gmail.com>
2019-07-05 23:36:07 +0700

    Add CategoryDao

---
+++ Model/Dao/CategoryDao.cs
@@ -0,0 +1,24 @@
+﻿using Model.EF;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.Dao
+{
+    public class CategoryDao
+    {
+        OnlineShopDbContext _context = null;
+
+        public CategoryDao()
+        {
+            _context = new OnlineShopDbContext();
+        }
+
+        public List<Category> ListAll()
+        {
+            return _context.Categories.Where(x => x.Status == true).ToList();
+        }
+    }
+}
