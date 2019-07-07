136d58d715314f6f00a29829fe43705f2ee37397
Dinh Hau <nguyendinhhau1807@gmail.com>
2019-07-07 22:20:02 +0700

    Add ProductCategory, update Index view

---
+++ Model/Dao/ProductCategoryDao.cs
@@ -0,0 +1,24 @@
+﻿using Model.EF;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.Dao
+{
+    public class ProductCategoryDao
+    {
+        OnlineShopDbContext _context = null;
+
+        public ProductCategoryDao()
+        {
+            _context = new OnlineShopDbContext();
+        }
+
+        public List<ProductCategory> ListAll()
+        {
+            return _context.ProductCategories.Where(x => x.Status == true).OrderBy(x => x.DisplayOrder).ToList();
+        }
+    }
+}
