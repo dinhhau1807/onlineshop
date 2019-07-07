41b94d0b1f18abf065557d1616c87b444911fabc
Dinh Hau <nguyendinhhau1807@gmail.com>
2019-07-07 16:51:14 +0700

    Add MenuDao

---
+++ Model/Dao/MenuDao.cs
@@ -0,0 +1,24 @@
+﻿using Model.EF;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.Dao
+{
+    public class MenuDao
+    {
+        OnlineShopDbContext _context = null;
+
+        public MenuDao()
+        {
+            _context = new OnlineShopDbContext();
+        }
+
+        public List<Menu> ListByGroupId(int groupId)
+        {
+            return _context.Menus.Where(x => x.TypeID == groupId).ToList();
+        }
+    }
+}
