b639e33c2f3a6d84eeb767825f2ba4a5269ef5ad
Dinh Hau <nguyendinhhau1807@gmail.com>
2019-07-10 01:33:30 +0700

    Pagination for /san-pham/...

--- Model/Dao/ProductDao.cs
+++ Model/Dao/ProductDao.cs
@@ -21,6 +21,11 @@ namespace Model.Dao
             return _context.Products.OrderByDescending(x => x.CreatedDate).Take(top).ToList();
         }
 
+        /// <summary>
+        /// List feature product
+        /// </summary>
+        /// <param name="top"></param>
+        /// <returns></returns>
         public List<Product> ListFeatureProduct(int top)
         {
             return _context.Products.Where(x => x.TopHot != null && x.TopHot > DateTime.Now).OrderByDescending(x => x.CreatedDate).Take(top).ToList();
@@ -30,6 +35,18 @@ namespace Model.Dao
         {
             var product = _context.Products.Find(productId);
             return _context.Products.Where(x => x.ID != productId && x.CategoryID == product.CategoryID).ToList();
+        }
+
+        /// <summary>
+        /// Get list product by category 
+        /// </summary>
+        /// <param name="categoryID"></param>
+        /// <returns></returns>
+        public List<Product> ListByCategoryId(long categoryID, ref int totalRecord, int pageIndex = 1, int pageSize = 2)
+        {
+            totalRecord = _context.Products.Where(x => x.CategoryID == categoryID).Count();
+            var model = _context.Products.Where(x => x.CategoryID == categoryID).OrderByDescending(x => x.CreatedDate).Skip((pageIndex - 1) * pageSize).Take(pageSize).ToList();
+            return model;
         }
 
         public Product ViewDetail(long id)
