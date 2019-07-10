67da4ac5e65502d0d351177e7d4c9873ec1bf3ce
Dinh Hau <nguyendinhhau1807@gmail.com>
2019-07-10 21:39:21 +0700

    Add CartItem Model

---
+++ OnlineShop/Models/CartItem.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OnlineShop.Models
+{
+    public class CartItem
+    {
+        public long ProductId { get; set; }
+        public int Quantity { get; set; }
+    }
+}
